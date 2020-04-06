2c410c5e1fe97b4e28fa17e45931ca62d64bafd3
Genevieve Allcroft <gennyallcroft@hotmail.co.uk>
2020-04-06 12:40:11 +0100

    log in and log out working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+
   get 'travel/index'
   get 'details/index'
   get 'home/index'
+
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+ get 'logout' => 'sessions#destroy'
 
   resources :responses
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_24_103023) do
+ActiveRecord::Schema.define(version: 2020_04_03_085142) do
 
   create_table "responses", force: :cascade do |t|
     t.string "name"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
