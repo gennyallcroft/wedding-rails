d2e54b83e2609d1c8c5c8f59b61afcbe91b74419
Genevieve Allcroft <gennyallcroft@hotmail.co.uk>
2020-04-16 16:34:32 +0100

    sign up page as welcome

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     end
 
     def authorized
-        redirect_to '/welcome' unless logged_in?
+        redirect_to '/users/new' unless logged_in?
     end
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_085142) do
+ActiveRecord::Schema.define(version: 2020_04_15_140240) do
 
   create_table "responses", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 2020_04_03_085142) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "surname"
+    t.string "email"
   end
 
 end
