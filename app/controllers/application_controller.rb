2a7f3265b30801961870620b62487f0fa0d3af48
Genevieve Allcroft <gennyallcroft@hotmail.co.uk>
2020-04-27 13:41:25 +0100

    viewing your rsvp working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
     before_action :authorized
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :current_response
+    helper_method :responded?
 
 
     def current_user    
         User.find_by(id: session[:user_id])  
+    end
+
+    def current_response    
+        Response.find_by(user_id: session[:user_id])  
+    end
+
+    def responded?
+        !current_response.nil?
     end
 
     def logged_in?
