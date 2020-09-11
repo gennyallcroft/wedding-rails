55d6ccd449049637ecfc4de0284329106b8dea20
Genevieve Allcroft <gennyallcroft@hotmail.co.uk>
2020-09-11 20:33:25 +0100

    refactor code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => "gennyandjoeywedding@gmail.com",
-  :password             => "vfjwoslbioqbzszf",
+  :user_name            => ENV["GMAIL_PASSWORD"],
+  :password             => ENV["GMAIL_PASSWORD"],
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
